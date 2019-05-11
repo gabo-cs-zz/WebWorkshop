91794562c97e902eede63d0f36c112081c85771a
Gabriel Coronado <gabrielomar2809@gmail.com>
2019-05-11 11:38:59 -0500

    CRUD Finished Editing and deleting working Toast msgs added

--- clase2.php
+++ clase2.php
@@ -18,56 +18,63 @@
 <body>
   <?php require_once 'process.php'; ?>
 
+  <?php if (isset($_SESSION['message'])) : ?>
+  <div class="alert alert-<?=$_SESSION['msg_type']?>">
+    <?php
+      echo $_SESSION['message'];
+      unset($_SESSION['message']);
+    ?>
+  </div>
+  <?php endif ?>
+
   <?php
     $mysqli = new mysqli('localhost', 'root', '', 'dbempleados') or die(mysqli_error($mysqli));
     $result = $mysqli->query(
-      "SELECT cedula_empleado, nombre_empleado, direccion_empleado, email_empleado, celular_empleado FROM empleados"
+      "SELECT id, cedula_empleado, nombre_empleado, direccion_empleado, email_empleado, celular_empleado FROM empleados"
     ) or die($mysqli->error);
-
-    function pre_r( $array ) {
-      echo '<pre>';
-      print_r($array);
-      echo '</pre>';
-    }
   ?>
 
   <div class="container mt-2">
     <h2 class="text-center" title="Author: Gabriel Coronado">Actualización de datos</h2>
-    <p class="text-center">Bienvenido, por favor diligencia toda la información que se presentan a continuación.</p>
+    <p class="text-center">Bienvenido, por favor diligencia la información que se presenta a continuación.</p>
     <form action="process.php" method="POST">
+      <input type="hidden" name="id" value="<?php echo $id;?>"/>
       <h6 class="mt-2">Datos personales</h6>
       <div class="datos">
         <div class="form-row mt-4">
           <div class="form-group col-md-4">
             <label for="inputCedula">Cédula: *</label>
-            <input type="number" name="cedula" class="form-control" id="inputCedula" required>
+            <input type="number" value="<?php echo $cedula; ?>" name="cedula" class="form-control" id="inputCedula" required autofocus>
           </div>
           <div class="form-group col-md-4">
             <label for="inputNombre">Nombre completo: *</label>
-            <input type="text" name="nombre" class="form-control" id="inputNombre" required>
+            <input type="text" value="<?php echo $nombre; ?>" name="nombre" class="form-control" id="inputNombre" required>
           </div>
           <div class="form-group col-md-4">
             <label for="inputDireccion">Dirección de residencia:</label>
-            <input type="text" name="direccion" class="form-control" id="inputDireccion">
+            <input type="text" value="<?php echo $direccion; ?>" name="direccion" class="form-control" id="inputDireccion">
           </div>
         </div>
         <div class="form-row">
           <div class="form-group col-md-6">
             <label for="inputCorreo">Correo electrónico: *</label>
-            <input type="email" name="correo" class="form-control" id="inputCorreo" required>
+            <input type="email" value="<?php echo $email; ?>" name="correo" class="form-control" id="inputCorreo" required>
           </div>
           <div class="form-group col-md-6">
             <label for="inputCel">Número celular:</label>
-            <input type="number" name="cel" class="form-control" id="inputCel">
+            <input type="number" value="<?php echo $cel; ?>" name="cel" class="form-control" id="inputCel">
           </div>
         </div>
-        <button type="submit" name="save" class="btn btn-dark">Enviar formulario</button>
+        <?php if ($update == true): ?>
+          <button type="submit" name="update" class="btn btn-dark">Enviar formulario</button>
+        <?php else: ?>
+          <button type="submit" name="save" class="btn btn-dark">Enviar formulario</button>
+        <?php endif; ?>
       </div>
     </form>
-    <div>
+    <div class="table-responsive">
       <h6 class="mt-4">Tabla de información</h6>
-      <table class="table datos">
-      <caption>Lista de empleados</caption>
+      <table class="table datos table-hover table-bordered">
         <thead>
           <tr>
             <th scope="col">Cédula</th>
@@ -75,7 +82,7 @@
             <th scope="col">Dirección</th>
             <th scope="col">Email</th>
             <th scope="col">Celular</th>
-            <th scope="col" colspan="2">Acción</th>
+            <th scope="col" colspan="2" class="text-center">Acciones</th>
           </tr>
         </thead>
         <tbody>
@@ -86,7 +93,12 @@
             <td><?php echo $row['direccion_empleado']; ?></td>
             <td><?php echo $row['email_empleado']; ?></td>
             <td><?php echo $row['celular_empleado']; ?></td>
-            <td></td>
+            <td>
+              <a href="clase2.php?edit=<?php echo $row['id']; ?>" class="text-secondary btn btn-link">Editar</a>
+            </td>
+            <td>
+              <a href="process.php?delete=<?php echo $row['id']; ?>" data-confirm="Are you sure?" class="text-danger btn btn-link" rel="nofollow" data-method="delete">Eliminar</a>
+            </td>
           </tr>
         <?php endwhile; ?>
         </tbody>
--- process.php
+++ process.php
@@ -1,6 +1,16 @@
 <?php
 
+session_start();
+
 $mysqli = new mysqli('localhost', 'root', '', 'dbempleados') or die(mysqli_error($mysqli));
+
+$update = false;
+$id = 0;
+$cedula = "";
+$nombre = "";
+$direccion = "";
+$email = "";
+$cel = "";
 
 if (isset($_POST['save'])){
   $cedula = $_POST['cedula'];
@@ -14,4 +24,54 @@ if (isset($_POST['save'])){
     (cedula_empleado, nombre_empleado, direccion_empleado, email_empleado, celular_empleado)
     VALUES('$cedula', '$nombre', '$direccion', '$email', '$cel')"
   ) or die($mysqli->error) ;
+
+  $_SESSION['message'] = "Empleado guardado exitosamente!";
+  $_SESSION['msg_type'] = "success";
+
+  header("location: clase2.php");
+}
+
+if (isset($_GET['delete'])){
+  $id = $_GET['delete'];
+  $mysqli->query("DELETE FROM empleados WHERE id=$id") or die($mysqli->error());
+
+  $_SESSION['message'] = "Empleado eliminado exitosamente!";
+  $_SESSION['msg_type'] = "danger";
+
+  header("location: clase2.php");
+}
+
+if(isset($_GET['edit'])){
+  $id = $_GET['edit'];
+  $update = true;
+  $result = $mysqli->query(
+    "SELECT id, cedula_empleado, nombre_empleado, direccion_empleado, email_empleado, celular_empleado FROM empleados WHERE id=$id"
+  ) or die($mysqli->error());
+  if ($result->num_rows > 0){
+    $row = $result->fetch_array();
+    $cedula = $row['cedula_empleado'];
+    $nombre = $row['nombre_empleado'];
+    $direccion = $row['direccion_empleado'];
+    $email = $row['email_empleado'];
+    $cel = $row['celular_empleado'];
+  }
+}
+
+if (isset($_POST['update'])){
+  $id = $_POST['id'];
+  $cedula = $_POST['cedula'];
+  $nombre = $_POST['nombre'];
+  $direccion = $_POST['direccion'];
+  $email = $_POST['correo'];
+  $cel = $_POST['cel'];
+  $mysqli->query(
+    "UPDATE empleados SET cedula_empleado='$cedula', nombre_empleado = '$nombre',
+    direccion_empleado = '$direccion', email_empleado = '$email',
+    celular_empleado = '$cel' WHERE id=$id"
+  ) or die($mysqli->error);
+
+  $_SESSION['message'] = "Información modificada exitosamente!";
+  $_SESSION['msg_type'] = "success";
+
+  header("location: clase2.php");
 }
