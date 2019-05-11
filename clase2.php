e33c9129fb7cc0cb81d9046ed9057b40e66b2a57
Gabriel Coronado <gabrielomar2809@gmail.com>
2019-05-11 11:54:34 -0500

    Data confirm added Alert confirmation added before deleting

--- clase2.php
+++ clase2.php
@@ -97,7 +97,7 @@
               <a href="clase2.php?edit=<?php echo $row['id']; ?>" class="text-secondary btn btn-link">Editar</a>
             </td>
             <td>
-              <a href="process.php?delete=<?php echo $row['id']; ?>" data-confirm="Are you sure?" class="text-danger btn btn-link" rel="nofollow" data-method="delete">Eliminar</a>
+              <input type="button" onClick="deleteRecord(<?php echo $row['id']; ?>)" class="text-danger btn btn-link" value="Eliminar" />
             </td>
           </tr>
         <?php endwhile; ?>
@@ -105,6 +105,14 @@
       </table>
     </div>
   </div>
+  <script>
+  function deleteRecord(id){
+    if(confirm("¿Seguro que desea eliminar este empleado?")){
+      window.location.href = 'process.php?delete=' + id + '';
+      return true;
+    }
+  }
+  </script>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
